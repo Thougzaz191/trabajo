35953d8fcba72f365c0a85190b6b503950e83bdb
Thougzaz191 <gabrielthekiler.1987@gmail.com>
2025-06-19 18:47:55 -0600

    mejore el codigo base para que no se confundan

--- #Trabajo 1 punto.py
+++ #Trabajo 1 punto.py
@@ -1,7 +1,60 @@
 #trabajo
+sw = 1
+def principal_menu():
+    try:
+        while sw == 1:
+            print("MENU PRINCIPAL")
+            print("1.Mantencion de maestros")
+            print("2.Inventario")
+            print("3.Produccion")
+            print("4.Ventas")
+            print("5.Salir")
+            op = int(input("Ingresa una opcion"))
+
+            if op == "1":
+                menu_maestros()
+
+            elif op == "2":
+                menu_inventario()
+                
+            elif op == "3":
+                menu_produccion()
+
+            elif op == "4":
+                menu_ventas()
+
+            elif op == "5":
+                sw = 0
+            else:
+                print("Opcion invaldia")
+    except ValueError:
+        print("Tiene que ingresar numeros, No letras ni otro signo")
 
 
-#Parte Benjamin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Parte Benjaming
 
 
 
@@ -13,6 +66,7 @@
 
 
 #Parte Vicente
+
 
 
 
